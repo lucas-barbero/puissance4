f986f06c6ade9f5499c6a246e0a84739ef3839b2
BosioAlexis <37872804+BosioAlexis@users.noreply.github.com>
2019-12-02 08:53:28 +0100

    Add files via upload

--- ia.pl
+++ ia.pl
@@ -30,37 +30,32 @@ testPlateau(Plateau,Couleur,Etat):- Etat = 0.
 
 
 
-listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):- eq(Couche,Profondeur), pair(Couche),
-                        enregistrerCoup(1,Plateau,Couleur,N1), testPlateau(N1,Couleur,E1), L1=[E1],
-                        enregistrerCoup(2,Plateau,Couleur,N2), testPlateau(N2,Couleur,E2),append(L1,[E2],L2),
-                        enregistrerCoup(3,Plateau,Couleur,N3), testPlateau(N3,Couleur,E3),append(L2,[E3],L3),
-                        enregistrerCoup(4,Plateau,Couleur,N4), testPlateau(N4,Couleur,E4),append(L3,[E4],L4),
-                        enregistrerCoup(5,Plateau,Couleur,N5), testPlateau(N5,Couleur,E5),append(L4,[E5],L5),
-                        enregistrerCoup(6,Plateau,Couleur,N6), testPlateau(N6,Couleur,E6),append(L5,[E6],L6),
-                        enregistrerCoup(7,Plateau,Couleur,N7), testPlateau(N7,Couleur,E7),append(L6,[E7],Liste).
+listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):-
+                    eq(Couche,Profondeur),
+                    testPlateau(Plateau,Couleur,E1), Liste=[E1].                    
 
-listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):- eq(Couche,Profondeur), \+pair(Couche), adversaire(Couleur,Adv),
-                        enregistrerCoup(1,Plateau,Adv,N1), testPlateau(N1,Couleur,E1), L1=[E1],
-                        enregistrerCoup(2,Plateau,Adv,N2), testPlateau(N2,Couleur,E2),append(L1,[E2],L2),
-                        enregistrerCoup(3,Plateau,Adv,N3), testPlateau(N3,Couleur,E3),append(L2,[E3],L3),
-                        enregistrerCoup(4,Plateau,Adv,N4), testPlateau(N4,Couleur,E4),append(L3,[E4],L4),
-                        enregistrerCoup(5,Plateau,Adv,N5), testPlateau(N5,Couleur,E5),append(L4,[E5],L5),
-                        enregistrerCoup(6,Plateau,Adv,N6), testPlateau(N6,Couleur,E6),append(L5,[E6],L6),
-                        enregistrerCoup(7,Plateau,Adv,N7), testPlateau(N7,Couleur,E7),append(L6,[E7],Liste).
+listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):-                     
+                    testPlateau(Plateau,Couleur,E1), 
+                    eq(E1,1), Liste=[1].
 
-listeValeur(Plateau,Couleur,Couche,Profondeur,L1):- 
-                                                        \+pair(Couche), C1 is Couche + 1, adversaire(Couleur,Adv),
+listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):- 
+                    testPlateau(Plateau,Couleur,E1), 
+                    eq(E1,-1), Liste=[-1].
+
+
+
+listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):- 
+                                                        \+pair(Couche), C1 is Couche + 1, adversaire(Couleur,Adv), 
     enregistrerCoup(1,Plateau,Couleur,N1), listeValeur(N1,Couleur,C1,Profondeur,L1), minListe(L1,M1),Li1=[M1],
     enregistrerCoup(2,Plateau,Couleur,N2), listeValeur(N2,Couleur,C1,Profondeur,L2), minListe(L2,M2),append(Li1,[M2],Li2),
     enregistrerCoup(3,Plateau,Couleur,N3), listeValeur(N3,Couleur,C1,Profondeur,L3), minListe(L3,M3),append(Li2,[M3],Li3),
     enregistrerCoup(4,Plateau,Couleur,N4), listeValeur(N4,Couleur,C1,Profondeur,L4), minListe(L4,M4),append(Li3,[M4],Li4),
     enregistrerCoup(5,Plateau,Couleur,N5), listeValeur(N5,Couleur,C1,Profondeur,L5), minListe(L5,M5),append(Li4,[M5],Li5),
     enregistrerCoup(6,Plateau,Couleur,N6), listeValeur(N6,Couleur,C1,Profondeur,L6), minListe(L6,M6),append(Li5,[M6],Li6),
-    enregistrerCoup(7,Plateau,Couleur,N7), listeValeur(N7,Couleur,C1,Profondeur,L7), minListe(L7,M7),append(Li7,[M7],Liste).
-
+    enregistrerCoup(7,Plateau,Couleur,N7), listeValeur(N7,Couleur,C1,Profondeur,L7), minListe(L7,M7),append(Li6,[M7],Liste).                    
 
 listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):- 
-                                                        pair(Couche), C1 is Couche + 1, adversaire(Couleur,Adv), 
+                                                        pair(Couche), C1 is Couche + 1, adversaire(Couleur,Adv),
     enregistrerCoup(1,Plateau,Adv,N1), listeValeur(N1,Couleur,C1,Profondeur,L1), maxListe(L1,M1),Li1=[M1],
     enregistrerCoup(2,Plateau,Adv,N2), listeValeur(N2,Couleur,C1,Profondeur,L2), maxListe(L2,M2),append(Li1,[M2],Li2),
     enregistrerCoup(3,Plateau,Adv,N3), listeValeur(N3,Couleur,C1,Profondeur,L3), maxListe(L3,M3),append(Li2,[M3],Li3),
@@ -68,8 +63,9 @@ listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):-
     enregistrerCoup(5,Plateau,Adv,N5), listeValeur(N5,Couleur,C1,Profondeur,L5), maxListe(L5,M5),append(Li4,[M5],Li5),
     enregistrerCoup(6,Plateau,Adv,N6), listeValeur(N6,Couleur,C1,Profondeur,L6), maxListe(L6,M6),append(Li5,[M6],Li6),
     enregistrerCoup(7,Plateau,Adv,N7), listeValeur(N7,Couleur,C1,Profondeur,L7), maxListe(L7,M7),append(Li6,[M7],Liste).
-     
 
+     
+%Pour chaque coup regarder listeValeur (Plateau,Couleur,1,6,Liste) Profondeur 6 c''est bien au dessus sest trop lent et 1 car on veut le coup d'apres
 % place le pion sur la colonne
 move(Colonne,Plateau, NewPlateau) :- enregistrerCoup(Colonne,Plateau,'X', NewPlateau).
 % vrai si placement possible sur la colonne (TODO)
