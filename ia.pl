9013b684fa138e7c3e4273ab396366de99ec1ec1
BosioAlexis <37872804+BosioAlexis@users.noreply.github.com>
2019-12-03 19:09:58 +0100

    Add files via upload

--- ia.pl
+++ ia.pl
@@ -28,19 +28,17 @@ value_col(Plateau, 5, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long)
 value_col(Plateau, 4, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long), value_col(Queue, 3, Value1), Value is Value1+3*Long .
 value_col(Plateau, 3, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long), value_col(Queue, 2, Value1), Value is Value1+2*Long.
 value_col(Plateau, 2, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long), value_col(Queue, 1, Value1), Value is Value1+Long.
-value_col(Plateau, 1, Value) :- Value = 0 .
+value_col(Plateau, 1, Value) :- Value = 0, !.
 
 
-testPlateau(Plateau,Couleur,Etat):- Plateau == [[4,0,4]], Etat = 0, !.
 testPlateau(Plateau,Couleur,Etat):- victoire(Plateau,Couleur), Etat = 1000.
-testPlateau(Plateau,Couleur,Etat):- adversaire(Couleur,Adv), victoire(Plateau,Adv), Etat = -1000.
+testPlateau(Plateau,Couleur,Etat):- adversaire(Couleur,Adv), victoire(Plateau,Adv), Etat = -1000, !.
 testPlateau(Plateau,Couleur,Etat):- value_col(Plateau, 7, Value), Etat = Value.
 
-testerCoup(C,Plateau,Couleur,NewPlateau):- nth1(C,Plateau,L), length(L,I), I < 6, enregistrerCoup(C,Plateau,Couleur,NewPlateau).
-testerCoup(C,Plateau,Couleur,NewPlateau):- NewPlateau = [[4,0,4]].
+testerCoup(C,Plateau,Couleur,NewPlateau):- nth1(C,Plateau,L), length(L,I), I < 8, enregistrerCoup(C,Plateau,Couleur,NewPlateau).
+testerCoup(C,Plateau,Couleur,NewPlateau):- NewPlateau = Plateau.
 %Donne la liste des valeurs des coups suivants
 
-listeValeur(Plateau,Couleur,Couche,Profondeur,Heuristiques):- Plateau == [[4,0,4]], Heuristiques = [0].
 
 listeValeur(Plateau,Couleur,Couche,Profondeur,Heuristiques):-
                     eq(Couche,Profondeur),
@@ -68,6 +66,7 @@ listeValeur(Plateau,Couleur,Couche,Profondeur,Heuristiques):-
     testerCoup(7,Plateau,Adv,N7), listeValeur(N7,Couleur,C1,Profondeur,L7), maxListe(L7,M7),append(Li6,[M7],Heuristiques), !.
 
 jouerCoup(N,Plateau,Couleur,NewPlateau) :- nth1(N,Plateau,L), length(L,I), I < 6, enregistrerCoup(N,Plateau,Couleur,NewPlateau), !.
+jouerCoup(N,Plateau,Couleur,NewPlateau) :- N > 6, N1 = 1, jouerCoup(N1,Plateau,Couleur,NewPlateau).
 jouerCoup(N,Plateau,Couleur,NewPlateau) :- N1 is N+1, jouerCoup(N1,Plateau,Couleur,NewPlateau).
 jouerCoup(N,Plateau,Couleur,NewPlateau) :- write("FIN").
 
@@ -76,23 +75,25 @@ jouerCoup(N,Plateau,Couleur,NewPlateau) :- write("FIN").
 jouerCoupIA(Plateau,Heuristiques,Couleur,NewPlateau) :-  maxListe(Heuristiques,M),  nth1(N,Heuristiques,M),  jouerCoup(N,Plateau,Couleur,NewPlateau), !.
 
 setProfondeur(Plateau,P,NP) :-
-    nth1(3,Plateau,L2), length(L2,I2), I2 > 3,nth1(4,Plateau,L3), length(L3,I3), I3 > 3, NP is P-2.
-setProfondeur(Plateau,P,NP) :- NP is P.    
+    nth1(4,Plateau,L4), length(L4,I4), I4>4, nth1(5,Plateau,L5), length(L5,I5), I5>4, 
+    P>2, NP is P-2, !.
+setProfondeur(Plateau,P,NP) :- NP is P, !.    
 
 jouerTourIA('X',B):- victoire(B,'O'), write("Victoire du joueur O").
 jouerTourIA('O',B):- victoire(B,'X'), write("Victoire du joueur X").
 jouerTourIA(_,B):- egalite(B), write("Egalite").
 
 jouerTourIA('O',B) :-
-                
-                    listeValeur(B,'O',1,3,Heuristiques),
+
+                    setProfondeur(B,5,NP),
+                    listeValeur(B,'O',1,NP,Heuristiques),
                     jouerCoupIA(B,Heuristiques,'O',NB),
                     afficherplateau(NB),
                     jouerTourIA('X',NB).
 
 jouerTourIA('X',B) :-
-                
-                    listeValeur(B,'X',1,3,Heuristiques),
+                    setProfondeur(B,5,NP),
+                    listeValeur(B,'X',1,NP,Heuristiques),
                     jouerCoupIA(B,Heuristiques,'X',NC),
                     afficherplateau(NC),
                     jouerTourIA('O',NC).
@@ -102,6 +103,7 @@ jouerTourIAJoueur('O',B):- victoire(B,'X'), write("Victoire du joueur X").
 jouerTourIAJoueur(_,B):- egalite(B), write("Egalite").
 
 jouerTourIAJoueur('O',B) :-
+
                     setProfondeur(Plateau,5,NP),
                     listeValeur(B,'O',1,NP,Heuristiques),
                     jouerCoupIA(B,Heuristiques,'O',NB),
@@ -124,7 +126,7 @@ jouerTourJoueurIA('O',B) :- repeat,
 							enregistrerCoup(C,B,'O', NB),
 							afficherplateau(NB),
 							jouerTourIAJoueur('O',NB).
-
+							
 jouerTourJoueurIA('X',B) :- repeat,
 							lireColonne('X',C),
 							verifierCoup(C,B),
@@ -136,60 +138,9 @@ jouerTourJoueurIA('X',B) :- repeat,
 %lancement du jeu
 puissance4IA:- afficherplateau([[],[],[],[],[],[],[]]),
                jouerTourIA('X',[[],[],[],[],[],[],[]]).
-
+			   
 puissance4JoueurIA:- afficherplateau([[],[],[],[],[],[],[]]),
                jouerTourJoueurIA('X',[[],[],[],[],[],[],[]]),
                afficherplateau([[],[],[],[],[],[],[]]).
-
-
-
-verif2CasesHorizontales(Plateau, Couleur, NumColonne, NbVictoirePossible) :-
-                   nth1(NumColonne,Plateau, Colonne),
-                   length(Colonne, Length),
-                   genererLigne(Length, Plateau, Ligne),
-                   verif2CasesHorizontaleRec1(Ligne, Couleur, NbVictoirePossible).
-
-
-verif2CasesHorizontaleRec1(Ligne, J, Cpt):-
-                   verif2CasesHorizontaleRec2(Ligne, J, Cpt1),
-                   estSousListeIncr([J,'-','-',J],Ligne, Incr),
-                   Cpt is Cpt1+Incr.
-
-verif2CasesHorizontaleRec2(Ligne, J, Cpt):-
-                   verif2CasesHorizontaleRec3(Ligne, J, Cpt1),
-                   estSousListeIncr([J,J,'-','-'],Ligne,Incr),
-                   Cpt is Cpt1+Incr.
-
-verif2CasesHorizontaleRec3(Ligne, J, Cpt):-
-                   verif2CasesHorizontaleRec4(Ligne, J, Cpt1),
-                   estSousListeIncr([J,'-',J,'-'],Ligne, Incr),
-                   Cpt is Cpt1+Incr.
-
-verif2CasesHorizontaleRec4(Ligne, J, Cpt):-
-                   verif2CasesHorizontaleRec5(Ligne, J, Cpt1),
-                   estSousListeIncr(['-',J,'-',J],Ligne, Incr),
-                   Cpt is Cpt1+Incr.
-
-verif2CasesHorizontaleRec5(Ligne, J, Cpt):-
-                   verif2CasesHorizontaleRec6(Ligne, J, Cpt1),
-                   estSousListeIncr(['-',J,J,'-'],Ligne, Incr),
-                   Cpt is Cpt1+Incr.
-
-verif2CasesHorizontaleRec6(Ligne, J, Cpt):-
-                   estSousListe(['-','-',J,J],Ligne),
-                   Cpt is 1.
-
-verif2CasesHorizontaleRec6(Ligne, J, Cpt):-
-                   \+estSousListe(['-','-',J,J],Ligne),
-                   Cpt is 0.
-
-
-estSousListeIncr(SousListe, Ligne, Incr) :-
-                   estSousListe(SousListe,Ligne),
-                   Incr is 1.
-
-estSousListeIncr(SousListe, Ligne, Incr) :-
-                   \+estSousListe(SousListe,Ligne),
-                   Incr is 0.
 
 
