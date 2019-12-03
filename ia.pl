bbd640009148772d94d9e198e16139a3006f50b6
BosioAlexis <37872804+BosioAlexis@users.noreply.github.com>
2019-12-03 12:23:42 +0100

    IA avec Heuristique

--- ia.pl
+++ ia.pl
@@ -19,7 +19,7 @@ minListe([X|Y], X):- minListe(Y, M), X <  M.
 pair(0).
 pair(X) :- X>0, X2 is X-2, pair(X2).
 
-eq(X,Y) :- Y=:=X, !.
+eq(X,Y) :- Y==X, !.
 
 testPlateau(Plateau,Couleur,Etat):- victoire(Plateau,Couleur), Etat = 1.
 testPlateau(Plateau,Couleur,Etat):- adversaire(Couleur,Adv), victoire(Plateau,Adv), Etat = -1.
@@ -71,27 +71,40 @@ listeValeur(Plateau,Couleur,Couche,Profondeur,Liste):-
     testerCoup(7,Plateau,Adv,N7), listeValeur(N7,Couleur,C1,Profondeur,L7), maxListe(L7,M7),append(Li6,[M7],Liste), !.
 
 
+value_col(Plateau, 7, Value) :- Plateau = [Colonne|Queue], value_col(Queue, 6, Value).
+value_col(Plateau, 6, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long),value_col(Queue, 5, Value1), Value is Value1+Long .
+value_col(Plateau, 5, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long),value_col(Queue, 4, Value1), Value is Value1+2*Long .
+value_col(Plateau, 4, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long), value_col(Queue, 3, Value1), Value is Value1+3*Long .
+value_col(Plateau, 3, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long), value_col(Queue, 2, Value1), Value is Value1+2*Long.
+value_col(Plateau, 2, Value) :- Plateau = [Colonne|Queue], length(Colonne, Long), value_col(Queue, 1, Value1), Value is Value1+Long.
+value_col(Plateau, 1, Value) :- Value = 0 .
+
+
+calculHeuristique(Plateau,Couleur,N,V,H) :- eq(V,-1), H = -1.
+calculHeuristique(Plateau,Couleur,N,V,H) :- nth1(N,Plateau,L), length(L,I), I > 5, H = -1.
+calculHeuristique(Plateau,Couleur,N,V,H) :- enregistrerCoup(N,Plateau,Couleur,NP), value_col(NP, 7,H).
+
+
+listerHeuristique(Plateau,Couleur,Liste,Heuristiques) :- maxListe(Liste,M), M > 0, Heuristiques=Liste, !.
+listerHeuristique(Plateau,Couleur,Liste,Heuristiques) :- minListe(Liste,M), M < 0, Heuristiques=Liste, !.
+listerHeuristique(Plateau,Couleur,Liste,Heuristiques) :- niemeElement(1,Liste,V1), niemeElement(2,Liste,V2), niemeElement(3,Liste,V3),
+                                                 niemeElement(4,Liste,V4), niemeElement(5,Liste,V5), niemeElement(6,Liste,V6), niemeElement(7, Liste, V7),  
+        calculHeuristique(Plateau,Couleur,1,V1,E1), L1=[E1],
+        calculHeuristique(Plateau,Couleur,2,V2,E2), append(L1,[E2],L2),
+        calculHeuristique(Plateau,Couleur,3,V3,E3), append(L2,[E3],L3),
+        calculHeuristique(Plateau,Couleur,4,V4,E4), append(L3,[E4],L4),
+        calculHeuristique(Plateau,Couleur,5,V5,E5), append(L4,[E5],L5),
+        calculHeuristique(Plateau,Couleur,6,V6,E6), append(L5,[E6],L6),
+        calculHeuristique(Plateau,Couleur,7,V7,E7), append(L6,[E7],Heuristiques), !.
+
+
 %Pour chaque coup regarder listeValeur (Plateau,Couleur,1,5,Liste) Profondeur 5 c''est bien au dessus sest trop lent et 1 car on veut le coup d''apres
 
-jouerCoupIA(Plateau,Couleur,NewPlateau) :-  maxListe(Liste,M), eq(M,1), nth1(N,Liste,1),  enregistrerCoup(N,Plateau,Couleur,NewPlateau), !.
+jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :-  listerHeuristique(Plateau,Couleur,Liste,Heuristiques), maxListe(Heuristiques,M),  nth1(N,Heuristiques,M),  enregistrerCoup(N,Plateau,Couleur,NewPlateau), !.
 
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :-   nth1(4,Plateau,L), length(L,I), I < 6, maxListe(Liste,M), nth1(4,Liste,M), enregistrerCoup(4,Plateau,Couleur,NewPlateau), !.
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :-   nth1(3,Plateau,L), length(L,I), I < 6, maxListe(Liste,M), nth1(3,Liste,M), enregistrerCoup(3,Plateau,Couleur,NewPlateau), !.
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :-   nth1(5,Plateau,L), length(L,I), I < 6, maxListe(Liste,M), nth1(5,Liste,M), enregistrerCoup(5,Plateau,Couleur,NewPlateau), !.
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :-   nth1(2,Plateau,L), length(L,I), I < 6, maxListe(Liste,M), nth1(2,Liste,M), enregistrerCoup(2,Plateau,Couleur,NewPlateau), !.
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :-   nth1(6,Plateau,L), length(L,I), I < 6, maxListe(Liste,M), nth1(6,Liste,M), enregistrerCoup(6,Plateau,Couleur,NewPlateau), !.
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :-   nth1(1,Plateau,L), length(L,I), I < 6, maxListe(Liste,M), nth1(1,Liste,M), enregistrerCoup(1,Plateau,Couleur,NewPlateau), !.
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :-   nth1(7,Plateau,L), length(L,I), I < 6, maxListe(Liste,M), nth1(7,Liste,M), enregistrerCoup(7,Plateau,Couleur,NewPlateau), !.
-
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :- maxListe(Liste,M), eq(M,-1), nth1(4,Liste,-1),  enregistrerCoup(4,Plateau,Couleur,NewPlateau).
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :- maxListe(Liste,M), eq(M,-1), nth1(3,Liste,-1),  enregistrerCoup(3,Plateau,Couleur,NewPlateau).
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :- maxListe(Liste,M), eq(M,-1), nth1(5,Liste,-1),  enregistrerCoup(5,Plateau,Couleur,NewPlateau).
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :- maxListe(Liste,M), eq(M,-1), nth1(2,Liste,-1),  enregistrerCoup(2,Plateau,Couleur,NewPlateau).
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :- maxListe(Liste,M), eq(M,-1), nth1(6,Liste,-1),  enregistrerCoup(6,Plateau,Couleur,NewPlateau).
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :- maxListe(Liste,M), eq(M,-1), nth1(1,Liste,-1),  enregistrerCoup(1,Plateau,Couleur,NewPlateau).
-jouerCoupIA(Plateau,Liste,Couleur,NewPlateau) :- maxListe(Liste,M), eq(M,-1), nth1(7,Liste,-1),  enregistrerCoup(7,Plateau,Couleur,NewPlateau), !.
-
-setProfondeur(Plateau,Profondeur,NewProfondeur) :- nth1(4,Plateau,L), length(L,I), I > 6, nth1(3,Plateau,L1), length(L1,I1), I1 > 6, NewProfondeur is Profondeur - 1.
+setProfondeur(Plateau,Profondeur,NewProfondeur) :- nth1(4,Plateau,L), length(L,I), I > 5, nth1(3,Plateau,L1), length(L1,I1), I1 > 5,Profondeur > 4, NewProfondeur is Profondeur - 1.
+setProfondeur(Plateau,Profondeur,NewProfondeur) :- nth1(4,Plateau,L), length(L,I), I > 5, nth1(3,Plateau,L1), length(L1,I1), I1 > 5,
+    nth1(2,Plateau,L2), length(L2,I2), I2 > 5, nth1(6,Plateau,L3), length(L3,I3), I1 > 5, Profondeur > 3, NewProfondeur is Profondeur - 1.
 setProfondeur(Plateau,Profondeur,NewProfondeur) :- NewProfondeur is Profondeur.
 
 jouerTourIA('O',B) :-
